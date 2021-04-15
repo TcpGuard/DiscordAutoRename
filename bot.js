c9a3cc7c8c6c16cf56c12750d0c3beae73ce6f07
Mohad <36363227+TcpGuard@users.noreply.github.com>
2021-04-15 06:01:13 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.on('ready' , ()=>{
+console.log(bot.user.tag)
+})
+const tag = `🎅🎉`
+const tag2 = `🎉`
+bot.on('message', msg  =>{
+    if(!msg.guild)return;
+    if(msg.content.startsWith('+command')){ //set command
+    msg.guild.members.forEach(m => m.setNickname(`${tag} ${m.user.username}`).then(err => console.log(err)))}
+})
+
+bot.on('guildMemberUpdate', (oldMember, newMember) => {
+if(newMember.user.bot)return;
+if(newMember.displayName.startsWith(`${tag} null`))return newMember.setNickname(`${tag} ` +newMember.user.username);
+if(newMember.displayName.startsWith(`${tag}`))return;
+if(oldMember.user.username != newMember.user.username){console.log('new username')}
+if(oldMember.nickname != newMember.nickname){
+    newMember.setNickname(`${tag} ${newMember.nickname}`).then(console.log('changed name'))}
+if(oldMember.user.username != newMember.user.username){
+    newMember.setNickname(`${tag} ${newMember.user.username}`).then(console.log('changed name'))}
+});
+bot.on('guildMemberAdd' , member =>{
+member.setNickname(`${tag} ${member.user.username}`)
+})
+
+
+bot.login("xx");
+
